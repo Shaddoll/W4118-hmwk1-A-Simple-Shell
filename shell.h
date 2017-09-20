eacccd0ac027642f2fe788c2b9f16089e16f6910
El Shaddai <chenzijian13@gmail.com>
2017-09-20 16:53:16 -0400

    add README

--- shell.h
+++ shell.h
@@ -5,11 +5,8 @@
 int runshell(void);
 ssize_t getcommand(char **lineptr, size_t *n);
 int parsecommand(char *line, char ***commands, size_t *num);
-int executecommand(char **commands, int pipe);
-int executepipe(char **commands, int n_pipe, int outfd);
 void concurrentpipe(char **commands, int n_pipe, int outfd);
 void history(char **argv, int fd);
-void removerecursion(char **commands, int n_pipe);
 void updatehistory(char *str);
 int str2number(char *str);
 int logerror(char *str);
